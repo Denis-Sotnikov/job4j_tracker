6522083af77b1f5615f5f51c72d8c24e912be655
Denchik-1988 <sport_piter@mail.ru>
2020-11-30 11:53:20 +0300

    3. Интерфейс Input [#413781]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,22 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    int countOfPassengers = 0;
+    @Override
+    public void drive() {
+        System.out.println("Автобус отправляется, держитесь за поручни.");
+    }
+
+    @Override
+    public void passengers(int countPassengers) {
+        this.countOfPassengers = countPassengers;
+    }
+
+    @Override
+    public int refuel(int litrs) {
+        int result = litrs * 44;
+        System.out.println("Bus refuel succes");
+        System.out.println("it cost " + result);
+        return result;
+    }
+}
