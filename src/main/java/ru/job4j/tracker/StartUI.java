99554d8bb481a819277c946fd0ac870bae9566e3
Denchik-1988 <sport_piter@mail.ru>
2020-12-01 03:01:25 +0300

    4.2. Статические методы. [#413786]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,6 +3,72 @@ package ru.job4j.tracker;
 
 
 public class StartUI {
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new Item ====");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+    public static void deleteItem(Input input, Tracker tracker){
+        int id = Integer.valueOf(input.askStr("Enter id: "));
+        if (tracker.delete(id)) {
+            System.out.println("Item delete success");
+        } else {
+            System.out.println("System error");
+        }
+    }
+    public static void replaceItem(Input input, Tracker tracker){
+        System.out.println("=== Edit item ====");
+        int id = Integer.valueOf(input.askStr("Enter id: "));
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        if (tracker.replace(id, item)) {
+            System.out.println("Item edit success");
+        } else {
+            System.out.println("System error");
+        }
+    }
+    public static void findAll(Tracker tracker){
+        System.out.println("=== Show all items ====");
+        Item[] array = tracker.findAll();
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+    public static void editItem(Input input, Tracker tracker){
+        System.out.println("=== Edit item ====");
+        int id = Integer.valueOf(input.askStr("Enter id: "));
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        if (tracker.replace(id, item)) {
+            System.out.println("Item edit success");
+        } else {
+            System.out.println("System error");
+        }
+    }
+    public static void findById(Input input, Tracker tracker){
+        System.out.println("=== Find item by Id ====");
+        int id = Integer.valueOf(input.askStr("Enter id: "));
+        Item item = tracker.findById(id);
+        if (item !=null){
+            System.out.println(item.toString());
+        }
+        else{
+            System.out.println("Заявка с таким id не найдена");
+        }
+    }
+    public static void findByName(Input input, Tracker tracker){
+        System.out.println("=== Find items by name ====");
+        String name = input.askStr("Enter name: ");
+        Item[] array = tracker.findByName(name);
+        if (array.length>0) {
+            for (int i = 0; i < array.length; i++) {
+                System.out.println(array[i].toString());
+            }
+        } else {
+            System.out.println("Заявки с таким именем не найдены");
+        }
+    }
 
     public void init(Input input, Tracker tracker) {
         boolean run = true;
@@ -10,60 +76,22 @@ public class StartUI {
             this.showMenu();
             int select = Integer.valueOf(input.askStr("Select: "));
             if (select == 0) {
-                System.out.println("=== Create a new Item ====");
-                String name = input.askStr("Enter name: ");
-                Item item = new Item(name);
-                tracker.add(item);
-            } else if (select==1) {
-                System.out.println("=== Show all items ====");
-                Item[] array = tracker.findAll();
-                for (int i = 0; i < array.length; i++) {
-                    System.out.println(array[i]);
-                }
+                StartUI.createItem(input, tracker);
+            } else if (select == 1) {
+                StartUI.findAll(tracker);
             } else if (select == 2) {
-                    System.out.println("=== Edit item ====");
-                    int id = Integer.valueOf(input.askStr("Enter id: "));
-                    String name = input.askStr("Enter name: ");
-                    Item item = new Item(name);
-                    if (tracker.replace(id, item)) {
-                        System.out.println("Item edit succes");
-                    } else {
-                        System.out.println("System eror");
-                    }
+                StartUI.editItem(input, tracker);
             }
-            else if (select == 3) {
-                System.out.println("=== Delete item ====");
-                int id = Integer.valueOf(input.askStr("Enter id: "));
-                if (tracker.delete(id)) {
-                    System.out.println("Item delete succes");
-                } else {
-                    System.out.println("System eror");
-                }
+              else if (select == 3) {
+                StartUI.deleteItem(input, tracker);
             }
-            else if (select == 4) {
-                System.out.println("=== Find item by Id ====");
-                int id = Integer.valueOf(input.askStr("Enter id: "));
-                Item item = tracker.findById(id);
-                if (item !=null){
-                    System.out.println(item.toString());
-                }
-                else{
-                    System.out.println("Заявка с таким id не найдена");
-                }
+              else if (select == 4) {
+                StartUI.findById(input, tracker);
             }
-            else if (select == 5) {
-                System.out.println("=== Find items by name ====");
-                String name = input.askStr("Enter name: ");
-                Item[] array = tracker.findByName(name);
-                if (array.length>0) {
-                    for (int i = 0; i < array.length; i++) {
-                        System.out.println(array[i].toString());
-                    }
-                } else {
-                    System.out.println("Заявки с таким именем не найдены");
-                }
+              else if (select == 5) {
+                StartUI.findByName(input, tracker);
             }
-             else if (select == 6) {
+              else if (select == 6) {
                 run = false;
             }
         }
