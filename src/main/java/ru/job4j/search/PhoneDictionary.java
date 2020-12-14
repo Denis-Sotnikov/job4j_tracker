01110230f423c06c399278e36ee8cb490e5126ee
Denis Sotnikov <sport_piter@mail.ru>
2020-12-14 20:46:10 +0300

    6. Функции высшего порядка [#413577]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
@@ -17,6 +18,48 @@ public class PhoneDictionary {
                     || p.getPhone().contains(key)
                     || p.getSurname().contains(key)) {
                 result.add(p);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Person> findOne(String key) {
+        Predicate<Person> findGetName = new Predicate<Person>() {
+            @Override
+            public boolean test(Person person) {
+                return person.getName().contains(key);
+            }
+        };
+
+        Predicate<Person> findGetPhone = new Predicate<Person>() {
+            @Override
+            public boolean test(Person person) {
+                return person.getPhone().contains(key);
+            }
+        };
+
+        Predicate<Person> findGetAddress = new Predicate<Person>() {
+            @Override
+            public boolean test(Person person) {
+                return person.getAddress().contains(key);
+            }
+        };
+
+        Predicate<Person> findGetSurname = new Predicate<Person>() {
+            @Override
+            public boolean test(Person person) {
+                return person.getSurname().contains(key);
+            }
+        };
+
+        Predicate<Person> combine = findGetName
+                .or(findGetAddress
+                .or(findGetPhone
+                .or(findGetSurname)));
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
             }
         }
         return result;
