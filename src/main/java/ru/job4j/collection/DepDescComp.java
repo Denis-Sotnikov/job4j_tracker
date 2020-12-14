fc49da142e553a70216491b9ce9697b533f9e07d
Denis Sotnikov <sport_piter@mail.ru>
2020-12-15 00:20:08 +0300

    2. Отсортировать департаменты [#413748]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,20 +7,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String left, String right) {
         String[] arrayLeft = left.split("/");
         String[] arrayRight = right.split("/");
-        int size = arrayLeft.length > arrayRight.length ? arrayRight.length : arrayLeft.length;
-        int result = 0;
-        for (int i = 0; i < size; i++) {
-            if (i == 0) {
-                result = arrayRight[i].compareTo(arrayLeft[i]);
-                if (result != 0) {
-                    return result;
-                }
-            }
-            result = arrayLeft[i].compareTo(arrayRight[i]);
-            if (result != 0) {
-                return result;
-            }
-        }
-        return Integer.compare(arrayLeft.length, arrayRight.length);
+        int result = arrayRight[0].compareTo(arrayLeft[0]);
+        return result != 0 ? result : left.compareTo(right);
     }
 }
