6e2d1f8d0527a9202ac3dbb27f51e342a9e7897e
Denis Sotnikov <sport_piter@mail.ru>
2020-12-16 14:55:32 +0300

    5. Преобразование матрицы чисел в список чисел. [#413595]

--- src/main/java/ru/job4j/stream/Matrix.java
+++ src/main/java/ru/job4j/stream/Matrix.java
@@ -8,20 +8,10 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Matrix {
-    public static void main(String[] args) {
-        Integer[][] matrix = new Integer[3][3];
-        matrix[0][0] = 0;
-        matrix[0][1] = 1;
-        matrix[0][2] = 2;
-        matrix[1][0] = 3;
-        matrix[1][1] = 4;
-        matrix[1][2] = 5;
-        matrix[2][0] = 6;
-        matrix[2][1] = 7;
-        matrix[2][2] = 8;
-        List<Integer> list = Arrays.stream(matrix)
-                .flatMap(array -> Stream.of(array))
+
+    public static List<Integer> change(Integer[][] array) {
+        return Arrays.stream(array)
+                .flatMap(ar -> Stream.of(ar))
                 .collect(Collectors.toList());
-        System.out.println(list);
     }
 }
---
+++ src/test/java/ru/job4j/stream/MatrixTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.stream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class MatrixTest {
+    private Integer[][] matrix = new Integer[3][3];
+    private List<Integer> expected = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        matrix[0][0] = 0;
+        matrix[0][1] = 1;
+        matrix[0][2] = 2;
+        matrix[1][0] = 3;
+        matrix[1][1] = 4;
+        matrix[1][2] = 5;
+        matrix[2][0] = 6;
+        matrix[2][1] = 7;
+        matrix[2][2] = 8;
+        expected.add(0);
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
+        expected.add(4);
+        expected.add(5);
+        expected.add(6);
+        expected.add(7);
+        expected.add(8);
+    }
+
+    @Test
+    public void testChange() {
+        List<Integer> input = Matrix.change(matrix);
+        assertThat(expected, is(input));
+    }
+
+}
