14ce72367b162fb186a5e2728f53a0c567cc72b0
Denis Sotnikov <sport_piter@mail.ru>
2020-12-22 02:56:31 +0300

    Задача к экзамену по теме "1. Экзамен. Lambda, Stream API, JDK Features. [#413609]"

---
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -0,0 +1,48 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EasyStream {
+
+    //private static List<Integer> listInside = new ArrayList<>();
+    private List<Integer> listForObject = new ArrayList<>();
+
+    public EasyStream(List<Integer> listForObject) {
+        this.listForObject = listForObject;
+    }
+
+    public static EasyStream of(List<Integer> source) {
+        //throw new UnsupportedOperationException();
+        //listInside = source;
+        return new EasyStream(source);
+    }
+
+    public EasyStream map(Function<Integer, Integer> fun) {
+        List<Integer> listForMap = new ArrayList<>();
+        for (int i = 0; i < this.listForObject.size(); i++) {
+            listForMap.add(fun.apply(this.listForObject.get(i)));
+        }
+        this.listForObject = listForMap;
+        return this;
+    }
+
+    public EasyStream filter(Predicate<Integer> fun) {
+        List<Integer> listForFilter = new ArrayList<>();
+        for (int i = 0; i < this.listForObject.size(); i++) {
+            if (fun.test(this.listForObject.get(i))) {
+                listForFilter.add(this.listForObject.get(i));
+            }
+        }
+        this.listForObject = listForFilter;
+        return this;
+    }
+
+    public List<Integer> collect() {
+//
+//        throw new UnsupportedOperationException();
+        return this.listForObject;
+    }
+}
