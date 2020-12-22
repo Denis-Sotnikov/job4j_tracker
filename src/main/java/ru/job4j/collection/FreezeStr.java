b194ab81c6cb1ad74e69ca4848b13f1e787be02e
Denis Sotnikov <sport_piter@mail.ru>
2020-12-22 16:28:33 +0300

    Задача №2 к экзамену по теме "Коллекции". Внесены корректировки на указанные комментарии ниже.
    "Вторая задача.
    
    1. Никаких массивов, списков и тем более сортировок там быть не должно. Решать нужно с помощью HashMap.
    
    2. Первое слово мы собираем в карту - Map<Character, Integer> - ключ это символ, значение - количество раз, которое этот символ встречается в слове.
    
    3. Потом мы просто пребираем второе слово - если значение по ключу равно 1, то удаляем такую пару, иначе - уменьшаем счтечик на 1
    
    4. Если карта на выходе пустая, значит слова одинаковые"

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -1,20 +1,35 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class FreezeStr {
     public static boolean eq(String left, String right) {
-        String[] leftArray = left.split("");
-        List<String> leftList = Arrays.asList(leftArray);
-        String[] rightArray = right.split("");
-        List<String> rightList = Arrays.asList(rightArray);
-        leftList.sort(Comparator.naturalOrder());
-        rightList.sort(Comparator.naturalOrder());
-        if (leftList.equals(rightList)) {
-            return true;
+        Map<Character, Integer> leftMap = new HashMap<>();
+        char[] leftArray = left.toCharArray();
+        char[] rightArray = right.toCharArray();
+        char keyLeft;
+        char keyRight;
+        for (int i = 0; i < leftArray.length; i++) {
+            keyLeft = leftArray[i];
+            if (!leftMap.containsKey(keyLeft)) {
+                leftMap.put(keyLeft, 1);
+            } else {
+                int count = leftMap.get(keyLeft);
+                leftMap.put(keyLeft, ++count);
+            }
         }
-        return false;
+
+        for (int i = 0; i < rightArray.length; i++) {
+            keyRight = rightArray[i];
+            if (leftMap.containsKey(keyRight)) {
+                if (leftMap.get(keyRight) == 1) {
+                 leftMap.remove(keyRight);
+                } else {
+                   int count = leftMap.get(keyRight);
+                   leftMap.put(keyRight, --count);
+                }
+            }
+        }
+        return leftMap.isEmpty() ? true : false;
     }
 }
