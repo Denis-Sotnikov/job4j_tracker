8ab327b6364b84e407821d507663575a9e3944f6
Denis Sotnikov <sport_piter@mail.ru>
2020-12-28 02:35:31 +0300

    9. Шаблоны проектирования в Stream API [#413599]

---
+++ src/main/java/ru/job4j/Car.java
@@ -0,0 +1,84 @@
+package ru.job4j;
+
+public class Car {
+    private String model;
+    private Integer productionYear;
+    private String manufacturer;
+    private Integer enginePower;
+    private String KP;
+    private Integer weight;
+
+    public static class BuilderCar {
+        private String model;
+        private Integer productionYear;
+        private String manufacturer;
+        private Integer enginePower;
+        private String KP;
+        private Integer weight;
+
+        public BuilderCar buildModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public BuilderCar buildProductionYear(Integer productionYear) {
+            this.productionYear = productionYear;
+            return this;
+        }
+
+        public BuilderCar buildManufacturer(String manufacturer) {
+            this.manufacturer = manufacturer;
+            return this;
+        }
+
+        public BuilderCar buildEnginePower(Integer enginePower) {
+            this.enginePower = enginePower;
+            return this;
+        }
+
+        public BuilderCar buildKP(String KP) {
+            this.KP = KP;
+            return this;
+        }
+
+        public BuilderCar buildWeight(Integer weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public Car build(){
+            Car car = new Car();
+            car.weight = weight;
+            car.enginePower = enginePower;
+            car.KP = KP;
+            car.manufacturer = manufacturer;
+            car.model = model;
+            car.productionYear = productionYear;
+            return car;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", productionYear=" + productionYear +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", enginePower=" + enginePower +
+                ", KP='" + KP + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Car car = new BuilderCar()
+                    .buildEnginePower(100)
+                    .buildKP("Mehanika")
+                    .buildManufacturer("Merceder")
+                    .buildProductionYear(2020)
+                    .buildWeight(2000)
+                    .buildModel("Raketa")
+                    .build();
+        System.out.println(car.toString());
+    }
+}
