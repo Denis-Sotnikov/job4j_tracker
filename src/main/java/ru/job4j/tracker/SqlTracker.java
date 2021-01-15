782d6b669e258e1ae8afad3aab272b6a175eebdf
Denis Sotnikov <sport_piter@mail.ru>
2021-01-15 06:35:40 +0300

    2. Трекер SQL [#437701]

---
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -0,0 +1,175 @@
+package ru.job4j.tracker;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class SqlTracker implements Store {
+    private Connection cn;
+
+    public void init() {
+        try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream(
+                "app.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            cn = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cn != null) {
+            cn.close();
+        }
+    }
+
+    @Override
+    public Item add(Item item) throws SQLException {
+        System.out.println(cn);
+        try (PreparedStatement statement =
+                     cn.prepareStatement("insert into items (name) VALUES (?)")) {
+            statement.setString(1, item.getName());
+            statement.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+
+    @Override
+    public boolean replace(String id, Item item) {
+        boolean result = false;
+        try (PreparedStatement statement =
+                     cn.prepareStatement("update items set name = ? where id = ?")) {
+            statement.setString(1, item.getName());
+            statement.setInt(2, Integer.parseInt(id));
+            statement.execute();
+            result = statement.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        boolean result = false;
+        try (PreparedStatement statement =
+                     cn.prepareStatement("delete from items where id = ?")) {
+            statement.setInt(1, Integer.parseInt(id));
+            statement.execute();
+            result = statement.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement statement = cn.prepareStatement("select * from items")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    items.add(new Item(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name")
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement statement = cn.prepareStatement("select * from items")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    if (key.equals(resultSet.getString("name"))) {
+                        items.add(new Item(
+                                resultSet.getInt("id"),
+                                resultSet.getString("name")
+                        ));
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    @Override
+    public Item findById(String id) {
+        Item it = new Item();
+        try (PreparedStatement statement =
+                     cn.prepareStatement("select * from items where id = ?")) {
+            statement.setInt(1, Integer.parseInt(id));
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    if (Integer.parseInt(id) == (resultSet.getInt("id"))) {
+                        it.setId(resultSet.getInt("id"));
+                        it.setName(resultSet.getString("name"));
+                    }
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return it;
+    }
+
+        public static void main(String[] args) throws SQLException {
+        Item it = new Item();
+        it.setName("Отправляемся");
+        it.setId(4);
+        System.out.println(it.getId());
+        Item itu = new Item();
+        itu.setName("Отправляемся в путешествие на юга");
+        it.setId(4);
+        System.out.println(it.getId());
+        //tracker.add()
+//        Input validate = new ValidateInput(
+//                new ConsoleInput()
+//        );
+        try (Store tracker = new SqlTracker()) {
+            tracker.init();
+
+            //tracker.add(it);
+
+            //System.out.println(tracker.replace("4", itu));sout
+
+            //System.out.println(tracker.delete("7"));
+
+//            for (Item i : tracker.findAll()) {
+//                System.out.println(i.getId() + " " + i.getName());
+//            }
+
+//            for (Item i : tracker.findByName("buy car")) {
+//                System.out.println(i.getId() + " " + i.getName());
+//            }
+
+            System.out.println(tracker.findById("1"));
+//            UserAction[] actions = {
+//                    new CreateAction()
+            //};
+            //new StartUI().init(validate, tracker, actions);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
