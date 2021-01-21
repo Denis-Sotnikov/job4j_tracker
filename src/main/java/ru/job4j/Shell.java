ff63eded87b544175a5effe87d5cb99d1b2f3ee4
Denis Sotnikov <sport_piter@mail.ru>
2021-01-21 20:22:51 +0300

    1. Экзамен. Ввод-вывод. [#437812] - задача к экзамену

--- src/main/java/ru/job4j/Shell.java
+++ src/main/java/ru/job4j/Shell.java
@@ -1,26 +1,34 @@
 package ru.job4j;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class Shell {
-    String paths = "";
+    private String paths = "";
 
     public void cd(String path) {
         String[] arr = path.split("/");
         for (int i = 0; i < arr.length; i++) {
             if (!arr[i].isEmpty()) {
-                paths = paths + "/" + arr[i];
+                if (!paths.endsWith("/")) {
+                    paths = paths + "/" + arr[i];
+                } else {
+                    paths = paths + arr[i];
+                }
             }
+        }
+        int q = 0;
+        if (paths.split("\\.\\.").length >= 2) {
+            q = paths.split("\\.\\.").length;
         }
         if (arr.length == 0) {
             paths = paths + path;
         }
         if (paths.endsWith("..")) {
-            System.out.println("yes");
             String buf = "";
             String[] array = paths.split("/");
-            for (int i = 0; i < array.length-2; i++) {
+            for (int i = 0; i < array.length - 2 - (q * 2 - 2); i++) {
                 if (!array[i].isEmpty()) {
                     buf = buf + array[i] + "/";
                 }
@@ -34,5 +42,16 @@ public class Shell {
 
     public String pwd() {
         return paths;
+    }
+
+    public static void main(String[] args) {
+        Shell shell = new Shell();
+        shell.cd("home/user/dir1/dir2/dir3/dir4/dir5/dir6/dir7");
+        System.out.println(shell.pwd());
+        shell.cd("..");
+        System.out.println(shell.pwd());
+        shell.cd("../../../../../../..");
+        System.out.println(shell.pwd());
+
     }
 }
