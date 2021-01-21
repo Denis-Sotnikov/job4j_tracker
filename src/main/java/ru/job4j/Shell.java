31b57176ad2dd8ab61f0bed7c59b3ab36f0f1838
Denis Sotnikov <sport_piter@mail.ru>
2021-01-21 21:01:20 +0300

    1. Экзамен. Ввод-вывод. [#437812] - дополнительная задача по экзамену

--- src/main/java/ru/job4j/Shell.java
+++ src/main/java/ru/job4j/Shell.java
@@ -1,47 +1,45 @@
 package ru.job4j;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import org.codehaus.plexus.util.CollectionUtils;
+
+import java.util.*;
 
 public class Shell {
-    private String paths = "";
+    List<String> listString = new ArrayList<>();
 
     public void cd(String path) {
+        if (listString.isEmpty()) {
+            listString.add("");
+        }
+
         String[] arr = path.split("/");
         for (int i = 0; i < arr.length; i++) {
             if (!arr[i].isEmpty()) {
-                if (!paths.endsWith("/")) {
-                    paths = paths + "/" + arr[i];
+                if (!listString.get(listString.size()-1).endsWith("/")) {
+                    listString.add("/" + arr[i]);
                 } else {
-                    paths = paths + arr[i];
+                    listString.add("/" + arr[i]);
                 }
             }
         }
         int q = 0;
-        if (paths.split("\\.\\.").length >= 2) {
-            q = paths.split("\\.\\.").length;
+        if (listString.toString().split("\\.\\.").length >= 2) {
+            q = listString.toString().split("\\.\\.").length - 1;
         }
-        if (arr.length == 0) {
-            paths = paths + path;
-        }
-        if (paths.endsWith("..")) {
-            String buf = "";
-            String[] array = paths.split("/");
-            for (int i = 0; i < array.length - 2 - (q * 2 - 2); i++) {
-                if (!array[i].isEmpty()) {
-                    buf = buf + array[i] + "/";
-                }
-            }
-            paths = buf;
-            if (paths.isEmpty()) {
-                paths = "/";
+
+        if (listString.get(listString.size()-1).endsWith("..")) {
+            int size = listString.size();
+            for (int i = listString.size() - 1; i > size - 3 - (q * 2 - 2); i--) {
+                listString.remove(i);
             }
         }
     }
 
     public String pwd() {
-        return paths;
+        return listString.toString()
+                .replaceFirst("\\[", "")
+                .replaceFirst("\\]", "")
+                .replaceAll(",", "");
     }
 
     public static void main(String[] args) {
