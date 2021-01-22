ea136db02bc22b020ab31285756e74a6d4251afd
Denis Sotnikov <sport_piter@mail.ru>
2021-01-22 12:53:58 +0300

    1. Экзамен. Ввод-вывод. [#437812]

--- src/main/java/ru/job4j/Shell.java
+++ src/main/java/ru/job4j/Shell.java
@@ -1,7 +1,5 @@
 package ru.job4j;
 
-import org.codehaus.plexus.util.CollectionUtils;
-
 import java.util.*;
 
 public class Shell {
@@ -12,44 +10,58 @@ public class Shell {
             listString.add("");
         }
 
-        String[] arr = path.split("/");
-        for (int i = 0; i < arr.length; i++) {
-            if (!arr[i].isEmpty()) {
-                if (!listString.get(listString.size()-1).endsWith("/")) {
-                    listString.add("/" + arr[i]);
-                } else {
-                    listString.add("/" + arr[i]);
-                }
-            }
-        }
-        int q = 0;
-        if (listString.toString().split("\\.\\.").length >= 2) {
-            q = listString.toString().split("\\.\\.").length - 1;
-        }
-
-        if (listString.get(listString.size()-1).endsWith("..")) {
+        if (path.equals("/")) {
             int size = listString.size();
-            for (int i = listString.size() - 1; i > size - 3 - (q * 2 - 2); i--) {
+            for (int i = size - 1; i > 1; i--) {
                 listString.remove(i);
             }
-        }
+        } else {
+            if (path.startsWith("c:/")) {
+                int size = listString.size();
+                for (int i = size - 1; i > 0; i--) {
+                    listString.remove(i);
+                }
+                listString.set(0, path);
+            } else {
+                if (!path.contains("..")) {
+                    String[] arr = path.split("/");
+                    for (int i = 0; i < arr.length; i++) {
+                        if (!arr[i].isEmpty()) {
+                            if (!listString.get(listString.size()-1).endsWith("/")) {
+                                listString.add("/" + arr[i]);
+                            } else {
+                                listString.add("/" + arr[i]);
+                            }
+                        }
+                    }
+                } else {
+                    int q = path.split("\\.\\.").length>0 ? path.split("\\.\\.").length : 1;
+                        int size = listString.size();
+                        for (int i = size - 1; i > size - 1 - q; i--) {
+                            listString.remove(i);
+                        }
+                }
+            }
+            }
     }
 
     public String pwd() {
         return listString.toString()
                 .replaceFirst("\\[", "")
                 .replaceFirst("\\]", "")
-                .replaceAll(",", "");
+                .replaceAll(",", "")
+                .replaceAll(" ", "");
     }
 
     public static void main(String[] args) {
         Shell shell = new Shell();
         shell.cd("home/user/dir1/dir2/dir3/dir4/dir5/dir6/dir7");
         System.out.println(shell.pwd());
-        shell.cd("..");
+//        shell.cd("/");
+        shell.cd("c:/");
+//        shell.cd("..");
         System.out.println(shell.pwd());
-        shell.cd("../../../../../../..");
+//        shell.cd("../../../../../../..");
         System.out.println(shell.pwd());
-
     }
 }
