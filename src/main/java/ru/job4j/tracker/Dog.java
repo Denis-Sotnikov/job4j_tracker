8ec6c1f84ad35dd36ca6dac8dd397de33f6af2e5
sport <sport_pite@mail.ru>
2026-07-07 21:36:49 +0300

    Создание класса

---
+++ src/main/java/ru/job4j/tracker/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        Dog a = new Dog();
+        Dog c = new Dog();
+    }
+}
