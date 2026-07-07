760d73bdc990d683aabe210360b16bdbaceb0822
sport <sport_pite@mail.ru>
2026-07-07 21:46:56 +0300

    Класс студент и метод для него

---
+++ src/main/java/ru/job4j/tracker/Student.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+public class Student {
+    public void music(){
+        System.out.println("Tra tra tra");
+    }
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.music();
+    }
+}
